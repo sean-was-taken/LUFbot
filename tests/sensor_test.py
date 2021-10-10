6c82b5254a74706e383d72b94706ea5d42101593
sean-was-taken <lelefacai@gmail.com>
2021-10-09 19:27:01 -0700

    update

--- tests/sensor_test.py
+++ tests/sensor_test.py
@@ -1,11 +1,12 @@
 from gpiozero import DistanceSensor
+from time import sleep
 
 ultrasonic = DistanceSensor(echo=15, trigger=14)
 
 avg = 0
 for x in range(10):
   avg += ultrasonic.distance
-
+  sleep(0.1)
 avg /= 10
 
 print(f"average distance from ground is: {avg} meters")
