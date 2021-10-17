d92bb14e55a6a8969ff68da296ea6613e030b822
Sean Yao <lelefacai@gmail.com>
2021-10-17 12:11:58 -0700

    Update and rename robot.py to test.py

---
+++ software/test.py
@@ -0,0 +1,24 @@
+if __name__ == "__main__":
+    print("This script must be imported")
+    exit()
+
+#import libraries
+import gpiozero
+import time
+
+# declare rover (base of the LUFbot)
+rover = gpiozero.Robot(left=(19, 26), right=(16, 20))
+rover.left()
+print("Left!")
+time.sleep(1)
+rover.right()
+print("Right!")
+time.sleep(1)
+rover.forward()
+print("Forward!")
+time.sleep(1)
+rover.backward()
+print("Backward!")
+time.sleep(1)
+rover.stop()
+print("Stopped!")
